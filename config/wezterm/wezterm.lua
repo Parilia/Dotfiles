fc769723d972334604d59a4bc1de9c07ec66ff58
Parilia <robbiehay1@gmail.com>
2023-10-07 18:18:33 +0100

    Update wezterm.lua

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,13 +4,21 @@ local config = {}
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 12.0
 config.color_scheme = "JetBrains Darcula"
+config.colors = {
+	cursor_bg = "#52b9c5",
+	cursor_border = "#52b9c5",
+	cursor_fg = "#eff0f1",
+	selection_bg = "#1a3272",
+	selection_fg = "#adadad",
+}
+
 config.window_background_opacity = 0.8
 config.hide_tab_bar_if_only_one_tab = true
 config.default_cursor_style = "BlinkingUnderline"
 config.cursor_blink_rate = 550
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
-
+-- window size
 config.initial_rows = 35
 config.initial_cols = 120
 
