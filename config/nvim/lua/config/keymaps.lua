76b27d160672eaf0a12abf77e6ced84be13a4fe1
Parilia <robbiehay1@gmail.com>
2023-11-04 12:47:07 +0000

    Delete config/nvim directory

--- config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set("n", "<leader>uL", "<cmd>set rnu<cr>", { desc = "Relative line numbering On" })
-
-vim.keymap.set("n", "<leader>uI", "<cmd>set rnu!<cr>", { desc = "Relative line numbering Off" })
